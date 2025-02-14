2b0e99bcd83462c097f58ef1b97b039bf0d0adc1
Gentrithotii <Gentrithoti@hotmail.com>
2025-02-14 16:02:58 +0100

    Added A.java file back

---
+++ src/A.java
@@ -0,0 +1,11 @@
+
+public class A
+{
+    private String aName;
+    private int age;
+
+    public  String getAName() { return aName; }
+    public void setAName(String aName){ this.aName = aName; }
+
+
+}
