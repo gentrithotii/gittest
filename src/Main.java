d8b046e28fbfc3b7c7b47709d431a127aa70bae1
Gentrithotii <Gentrithoti@hotmail.com>
2025-02-17 15:48:13 +0100

    Jobs done :)

--- src/Main.java
+++ src/Main.java
@@ -4,18 +4,15 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        boolean aLoopi = false;
+
         System.out.println("Hello and welcome!");
-//        int menuPicked = 0;
         ArrayList<A> aList = new ArrayList<>();
         int numberOfThingsToAdd = 0;
-//        A newAItem = new A();
-//        newAItem.setAName("Gentrit");
-//        newAItem.setAge(1);
 
         Scanner userInput = new Scanner(System.in);
 
         System.out.print("Enter the amount of items you want to add: ");
+
         try{
         String stringNum = userInput.nextLine();
         numberOfThingsToAdd = Integer.parseInt(stringNum);
@@ -43,25 +40,6 @@ public class Main {
         for(A aItems : aList){
             System.out.print( " " + aItems.getAName() + " " + aItems.getAge() );
         }
-       /* while(!loopi){
-            Scanner getNameForObject = new Scanner(System.in);
-            System.out.print("Enter the name you want to give to the object: ");
-            String objectName = getNameForObject.nextLine();
-            A aPerson = new A();
-            aPerson.setAName(objectName);
-            String test = aPerson.getAName().toLowerCase();
-            String keyWord = "Gentrit".toLowerCase();
-        for (int i = 1; i <= 5; i++) {
-            if(test.contains(keyWord)){
-            System.out.println("i = " + i);
-            loopi = true;
-            }
-        }
-        }
-        */
-
-
-//        int [] numbers = {1, 3, 4, 6, 7, 2, 9, 12, 1, 52};
 
     }
 }
